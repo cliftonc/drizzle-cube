755aa26732a4cb3b48de04f53860d2c547a1c84b
Clifton Cunningham <clifton.cunningham@gmail.com>
2026-03-31 07:17:46 +0100

    Remove file

--- vitest.nextjs.local.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    include: ['tests/adapters/nextjs.test.ts'],
-    environment: 'node',
-    globals: true,
-  },
-})
